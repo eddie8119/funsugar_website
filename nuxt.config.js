f0cc21c21cdec104aef9278153ed0b13bb59b039
eddie <wang8119@gmail.com>
2026-01-28 21:17:58 +0800

    chore

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,25 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '專業空間攝影｜方生糖',
+    title: "專業空間攝影｜方生糖",
     htmlAttrs: {
-      lang: 'Zh-Hant-TW'
+      lang: "Zh-Hant-TW",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '台北室內設計空間攝影師，成為你的室內設計行銷利器' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "台北室內設計空間攝影師，成為你的室內設計行銷利器",
+      },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/main.css',
-  ],
+  css: ["@/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -30,13 +30,10 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxt/postcss8',
-  ],
+  buildModules: ["@nuxt/postcss8"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
@@ -46,5 +43,5 @@ export default {
         autoprefixer: {},
       },
     },
-  }
-}
+  },
+};
