35322f58376738e33a591a65db7e631869522176
eddie <wang8119@gmail.com>
2026-03-01 23:50:23 +0800

    素材

--- constants/link.ts
+++
@@ -1,8 +0,0 @@
-export const BRAND_LINK = {
-  email: 'funsugar8119@gmail.com',
-  website: 'https://designer-helper-funsugar.netlify.app/',
-  instagram: 'https://www.instagram.com/funsugar_not_designer/',
-  youtube: 'https://www.youtube.com/playlist?list=PLq6qa78AJYu004VTivvUU-RY3Hc6pEzlF',
-  behance: "https://www.behance.net/WANG81195321",
-  line: "https://line.me/R/ti/p/eddi7777",
-};
